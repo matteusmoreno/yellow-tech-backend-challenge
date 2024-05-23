710187e7f5926d3416d3c0c14916d422f692dcad
Matteus Moreno <matteus_moreno@live.com>
2024-05-23 11:54:16 -0300

    inicio do projeto

---
+++ src/main/java/br/com/matteusmoreno/yellow_tech_backend_challenge/YellowTechBackendChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.matteusmoreno.yellow_tech_backend_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YellowTechBackendChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YellowTechBackendChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/matteusmoreno/yellow_tech_backend_challenge/YellowTechBackendChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matteusmoreno.yellow_tech_backend_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YellowTechBackendChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
